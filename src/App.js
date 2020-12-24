fa6d87d350aabf65ac8045f1a832e51f65ab10f3
Oybek <oybektoshmatov95@gmail.com>
2020-12-25 01:41:52 +0900

    committing half built web app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import NewsCards from "./components/NewsCards/NewsCards";
 
+const alanKey =
+  "e079af19a1f6289950925f5a33bba8a12e956eca572e1d8b807a3e2338fdd0dc/stage";
 function App() {
+  const [newsArticles, setNewsArtciles] = useState([]);
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles }) => {
+        if (command === "newsHeadlines") {
+          setNewsArtciles(articles);
+        }
+      },
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Alan AI News Application</h2>
+      <NewsCards articles={newsArticles} />
     </div>
   );
 }
